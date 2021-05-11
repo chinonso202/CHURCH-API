9adbfc8033cbc43d0fd67155e3e7e766726cf3de
chinonso202 <chinonsopius88@yahoo.com>
2021-05-11 01:28:07 +0100

    church commit

---
+++ src/components/Home.js
@@ -0,0 +1,213 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            
+
+	<div id="colorchanger">
+           <a class="colorbox colorbrown" href="?theme=brown" title="brown Theme"> </a>
+           <a class="colorbox coloryellow" href="?theme=yellow" title="yellow Theme"> </a>
+       </div>
+
+
+<div class="banner">
+	 <div class="container">		 
+		 <div class="logo">			 
+			 <a href="index.html"><span></span></a>
+		 </div>
+		 <div class="top-menu">
+			 <span class="menu"> </span>
+			 <ul>
+			 <li><a href="index.html">HOME</a></li>
+			 <li><a href="about.html">ABOUT</a></li>
+			 <li><a href="ministries.html">MINISTRIES</a></li>
+			 <li><a href="404.html">EVENTS</a></li>
+			 <li><a href="contact.html">CONTACT</a></li>
+			 <div class="clearfix"></div>
+			 </ul>
+			
+			 </div>
+		 </div>
+	 </div>
+
+<div class="content">
+	 <div class="container">
+		
+		 <div class="col-md-3 content-left">
+			 <div class="bird">
+				 <a href="ministries.html"/><i class="pop"></i>
+				 <i class="icon1"></i>
+				 <h4>Joh3:116</h4>
+				 <h3>for God so love the world..</h3>
+				 <div class="clearfix"></div>
+			 </div>
+			 <div class="statue">
+				  <a href="ministries.html"><i class="pop"/></a>
+				 <i class="icon2"></i>
+				 <h4>matt3:33</h4>
+				 <h3>seek first the kingdom of God and his righteousness...</h3>
+				 <div class="clearfix"></div>
+            
+                 
+                 
+			 </div>
+             
+			 <div class="publish">
+				 <h3 class="info">worship with us</h3>
+				 <div class="publish-grids">
+					 <div class="publish-grid">
+						 <div class="date">
+							 <span>07</span>
+							 <h6>August</h6>
+						 </div>
+						 <div class="date-info">
+						 <h4><a href="#">prayer for Nigeria</a></h4>
+						 <p>pray for the peace of jerusalem, when iit prosper, you will also prosper</p>
+						 </div>
+						<div class="clearfix"></div>
+					 </div>
+					 <div class="publish-grid">
+						 <div class="date">
+							 <span>08</span>
+							 <h6>August</h6>
+						 </div>
+						 <div class="date-info">
+						 <h4><a href="#">manna water</a></h4>
+						 <p>come with a bottle of water and anointing oil</p>
+						 </div>
+						<div class="clearfix"></div>
+					 </div>
+					 <div class="publish-grid">
+						 <div class="date">
+							 <span>09</span>
+							 <h6>August</h6>
+						 </div>
+						 <div class="date-info">
+						 <h4><a href="#">Glorious service</a></h4>
+						 <p>come and experience God</p>
+						 </div>
+						<div class="clearfix"></div>
+					 </div>	
+				 </div>
+				 
+				 <div class="publish-contact">
+					 <h3>JESUS CARE</h3>
+					 <ul>
+						 <li><span class="icon1"></span></li>
+						 <li><a href="mailto:infomail@example.com">info@shiloh.com</a></li>
+					 </ul>
+					 <ul>
+						 <li><span class="icon2"></span></li>
+						 <li><a href="#">(000) 888 8888 88888</a></li>
+					 </ul>
+					 <ul>
+						 <li><span class="icon3"></span></li>
+						 <li><a href="#">Facebook</a></li>
+					 </ul>
+					 <ul>
+						 <li><span class="icon4"></span></li>
+						 <li><a href="#">Twitter</a></li>
+					 </ul>
+					 <ul>
+						 <li><span class="icon5"></span></li>
+						 <li><a href="#">Linkedin</a></li>
+					 </ul>
+				 </div>
+				 <div class="clearfix"></div>
+			 </div>
+		 </div>
+	
+		 <div class="col-md-6 content-middle">
+			 <div class="middle-banner">
+				 <div class="banner-info">
+				 <h3>GOD IS LOVE</h3>
+				 <h4>CAST ALL YOUR WORRIES UPON HIM</h4>
+				 <a class="btn btn-1 btn-1b" href="#">MORE INFO</a>
+				 </div>
+			 </div>
+			 <div class="content-sec">
+				 <div class="placer">
+					 <div class="placer-pic"><img src="images/pic.jpg" alt=""/></div>
+					 <div class="placer-head">
+						 <h3>THE LORD IS GOD</h3>
+						 <p>JOHN10:110 </p>
+					 </div>
+					 <div class="clearfix"></div>
+					 <p class="placer-info"> NEED COUNSELLING, PLEASE REACH OUT TO US </p>
+					 <a class="btn btn-1 btn-1b" href="#">MORE INFO</a>
+				 </div>	
+				 <div class="content-grids">
+					 <h3>JESUS IS COMING SOON</h3>
+					 <p>1CORINTHIANS 6:19</p>
+					 <a class="btn btn-1 btn-1b" href="#">MORE INFO</a>
+				 </div>
+				 <div class="content-grids">
+					 <h3>ANNUAL 10DAYS FASTING AND PRAYER PROGRAM WILL BE COMING UP ON JUNE1-10 2021</h3>
+					 <img src="images/pic2.jpg" alt=""/>
+					 <p>COME FASTING,COME EXPECTING</p>
+					 <a class="btn btn-1 btn-1b" href="#">MORE INFO</a>
+				 </div>
+			 </div>
+		 </div>
+		
+		 <div class="col-md-3 content-right">
+			 <div class="path">
+				  <a href="ministries.html"/><i class="pop2"></i>
+				<i class="icon3"></i>
+				 <h4>MY FATHER HOUSE SHALL BE CALLED A PLACE OF "PRAYER"</h4>
+				 <h3>HEB 11:4</h3>
+				 <div class="clearfix"></div>
+			 </div>
+			 <div class="holly">
+				 <a href="ministries.html"/> <i class="pop3"></i>
+				 <i class="icon4"></i>
+				 <h4>REPENT NOW</h4>
+				 <h3>JESUS IS COMING SOON</h3>
+				 <div class="clearfix"></div>
+			 </div>
+			 <div class="content-right-sec">
+				 <div class="search">
+					 <h3>SEARCH</h3>
+					 <form>
+					 <input type="text" value="search" onfocus="this.value=''" onblur="this.value='search'"></input>
+					 <input type="submit" value=""/>
+					 </form>
+				 </div>
+				 <div class="content-right-list">
+					 <h3>FOR PRAYERS AND COUNSELLING, REACH OUT TO OUR TEAMS</h3>
+					 <ul>
+						 <li><a href="#">EVERGALISM GROUP</a></li>
+						 <li class="active"><a href="#">PRAYER WARRIORS</a></li>
+						 <li><a href="#">TECHNICAL GROUP</a></li>
+						 <li><a href="#">CHOIRS</a></li>
+						 <li><a href="#">WE CARE MINISTRY</a></li>
+					 </ul>
+				 </div>
+				 <div class="content-right-contact">
+						<h3>ENTER YOUR CONTACT DETAILS</h3>
+					 <form>
+							<input type="text" class="text" value="Enter your name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your name';}"/>
+							<input type="text" class="text" value="Enter your email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your email';}"/>
+							<input type="text" class="text" value="Enter your phone" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your phone';}"/>
+							<textarea value="Message" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your message';}">Enter your message....</textarea>					   
+							<input name="submit" type="submit" value="SUBMIT"/>
+					 </form>
+				 </div>
+			 </div>
+		 </div>
+		 
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+
+<div class="fotter">
+	 	 <p>Copyrights &copy; 2015 SHILOH MINISTRY All rights reserved | Template by <a href="http://SHILOHMINISTRY.COM">CHINONSO</a></p> 
+</div>
+
+
+        </div>
+    )
+}
+
+export default Home
